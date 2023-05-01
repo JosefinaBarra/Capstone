1ccbebc0e9cea59c432a03b7a7b3489df9b6c6da
JosefinaBarra <josefina.barra@uc.cl>
2023-05-01 18:19:52 -0400

    Comentarios en simulacion.py

--- simulacion.py
+++ simulacion.py
@@ -11,6 +11,7 @@ class Bodega:
     def __init__(
         self, env, rop, max, costo_almacenamiento, costo_orden, precio_venta, lead_time
     ):
+        # Condiciones iniciales de bodega
         self.env = env
         self.rop = rop
         self.max = max
@@ -23,10 +24,10 @@ class Bodega:
         # Inventario inicial
         self.inventario = max
 
-        # Condiciones iniciales
         self.balance = 0
         self.cant_ordenada = 0
         
+        # Listas de datos para guardar
         self.obs = []
         self.nivel_inventario = []
         self.estado_balance = []
@@ -39,8 +40,11 @@ class Bodega:
         return np.random.randint(1,5)
 
     def realizar_orden(self):
+        ''' Se crea y espera el pedido para la bodega '''
         # Política (s,S) o min/max:
         self.cant_ordenada = self.max
+
+        # Costo por orden realizada
         self.balance -= self.cant_ordenada * self.costo_orden
         print(f"[{round(self.env.now, 2)}] Nueva orden por: {self.max}")
         print(f' - Costos por esta orden: {self.cant_ordenada * self.costo_orden} - Balance: {self.balance}\n')
@@ -50,12 +54,17 @@ class Bodega:
         
         # Espero que llegue pedido
         yield self.env.timeout(self.lead_time)
+
+        # Actualizo stock
         self.inventario += self.cant_ordenada
+
+        # No hay ordenes pendientes
         self.cant_ordenada = 0
 
         print(f'[{round(self.env.now, 2)}] Orden recibida, {self.inventario} en inventario\n')
 
     def runner_setup(self):
+        ''' Simula el funcionamiento de la bodega '''
         while True:
             llegada_cliente = self.generar_llegada_cliente()
 
@@ -72,7 +81,7 @@ class Bodega:
             # Generar demanda cliente
             self.demanda = self.generar_demanda()
 
-            # Hay stock
+            # Si hay stock
             if self.demanda < self.inventario:
                 self.balance += self.precio_venta*self.demanda
                 self.inventario -= self.demanda
@@ -82,7 +91,7 @@ class Bodega:
                 print(f' - Balance: {self.balance}')
                 print(f' - Inventario: {self.inventario}\n\n')
             
-            # Vendo todo -> Quedo sin stock
+            # Stock justo -> Vendo todo -> Quedo sin stock
             elif self.demanda == self.inventario:
                 self.balance += self.precio_venta*self.demanda
                 self.inventario -= self.demanda
@@ -92,15 +101,17 @@ class Bodega:
                 print(f' - Balance: {self.balance}')
                 print(f' - Inventario: {self.inventario}\n\n')
             
+            # Stock insuficiente
             elif self.demanda > self.inventario:
                 print(f'[{round(self.env.now)}] NO TENEMOS STOCK!!\n\n')
 
-            # Revisión continua: Inventario < min Y no hay orden en camino
+            # Revisión continua: Si Inventario < min Y no hay pedido en camino
             if self.inventario < self.rop and self.cant_ordenada == 0:
                 print(f'REVISANDO STOCK: REALIZO ORDEN')
                 self.env.process(self.realizar_orden())
     
     def observe(self):
+        ''' Revisión continua del funcionamiento de la bodega '''
         while True:
             self.obs.append(self.env.now)
             self.nivel_inventario.append(self.inventario)
@@ -108,6 +119,7 @@ class Bodega:
             yield self.env.timeout(0.1)
     
     def guardar_datos(self):
+        ''' Exporto datos de la bodega en archivo excel '''
         col1 = "Tiempo"
         col2 = "Inventario actual"
         col3 = "Balance"
@@ -120,6 +132,7 @@ class Bodega:
         data.to_excel('sample_data.xlsx', sheet_name='sheet1', index=False)
     
     def grafico(self):
+        ''' Crea gráfico día vs inventario '''
         plt.figure()
         plt.step(self.obs, self.nivel_inventario, where='post')
         plt.xlabel("Días")
@@ -144,6 +157,12 @@ s = Bodega(
     precio_venta=100,
     lead_time=2
 )
-run(s,8)
+
+# Corro simulación de bodega hasta tiempo = 8
+run(s,until=8)
+
+# SE genera el gráfico
 s.grafico()
+
+# Guardo datos
 s.guardar_datos()
