5cf573ec1fcbd9ad9ab384ecacfaeebfef1c7b05
JosefinaBarra <josefina.barra@uc.cl>
2023-05-23 15:22:51 -0400

    heatmap

---
+++ guardar_data.py
@@ -0,0 +1,83 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def guardar_pares_kpi(valores_politica, resultado, replicas, excel):
+    data_excel = {}
+    for valores in valores_politica:
+        #print(f"\nPOLÍTICA {valores}")
+        resultado_kpi = resultado[str(valores)]
+        rows = []
+        for j in range(0, replicas):
+            kpi = resultado_kpi["repeticion"+str(j)]
+            rows.append(list(kpi.values()))
+        
+        df = pd.DataFrame(rows, columns = list(kpi.keys()))
+        #df.to_excel(excel, sheet_name='kpi'+str(valores), index=True)
+
+        nombre_columna = list(df.columns.values)
+        columna = []
+        for i in range(0, len(nombre_columna)):
+            valores_kpi = df[nombre_columna[i]].describe().loc[['mean']].tolist()
+            valores_kpi = map(str,valores_kpi)
+            columna.append(str(''.join(valores_kpi)))
+            data_excel[str(valores)] = columna
+
+        #df.describe().loc[['mean','min','max']].to_excel(excel, sheet_name='kpi'+str(valores), index=True)
+
+    # KPI por columna y cada fila tiene pares (s,S)
+    df2 = pd.DataFrame(data_excel).transpose()
+    df2.columns = nombre_columna
+    df2.to_excel(excel, sheet_name='Mean', index=True)
+
+    return nombre_columna, data_excel
+
+def guardar_matriz_heatmap_kpi(nombre_columna, rango_s_S, data_excel, excel):
+    actual_path = os.getcwd()
+    for kpi in range(0, len(nombre_columna)):
+        matriz = []
+        for i in range(rango_s_S):
+            fila = []
+            for j in range(rango_s_S):
+                if i <= j:
+                    tupla = "("+str(i)+", "+str(j)+")"
+                    dato = data_excel[tupla][kpi]
+                    fila.append(dato)
+                else:
+                    fila.append('0')
+                fila_int = [float(x) for x in fila]
+            matriz.append(fila_int)
+
+        df3 = pd.DataFrame(matriz)
+        df3.to_excel(excel, sheet_name="Mean-kpi"+str(kpi), index=True)
+
+        fig, ax = plt.subplots()
+        im = ax.imshow(matriz, cmap='GnBu')
+        fig.colorbar(im, ax=ax)
+
+
+        # Show all ticks and label them with the respective list entries
+        ax.xaxis.tick_top()
+        ax.set_xticks(np.arange(0, rango_s_S))
+        ax.set_yticks(np.arange(0, rango_s_S))
+
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), ha="right",
+                rotation_mode="anchor")
+
+        # Loop over data dimensions and create text annotations.
+        for i in range(rango_s_S):
+            for j in range(rango_s_S):
+                text = ax.text(j, i, np.round(matriz[i][j],3),
+                            ha="center", va="center")
+
+        ax.set_title(str(nombre_columna[kpi]))
+        fig.tight_layout()
+        
+        folder = 'graficos'
+        dir = os.path.join(actual_path, folder)
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        fig.savefig(folder+'/kpi'+str(kpi)+".png")
+    
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import csv
 from simulacion_diario import Bodega
 from funciones import histogramas_png, generar_demanda
 #from abrir_archivo import demanda
+from guardar_data import guardar_pares_kpi, guardar_matriz_heatmap_kpi
 
 
 np.random.seed(0)
@@ -17,7 +18,7 @@ excel = pd.ExcelWriter(
     engine_kwargs={"options": {"strings_to_numbers": True}}
 )
 
-replicas = 1000
+replicas = 2
 periodos = 30
 politica = "(s,S)"
 #politica = "EOQ"   
@@ -28,10 +29,12 @@ resultado_base = {}
 caso_base = True
 no_mostro_grafico_base = True
 
+rango_s_S = 4
+
 # Demanda real
 #print(demanda)
 
-valores_politica = [(s, S) for s in range(51) for S in range(51) if s<=S]
+valores_politica = [(s, S) for s in range(rango_s_S) for S in range(rango_s_S) if s<=S]
 
 # Demanda simulada
 demanda = generar_demanda(periodos)
@@ -64,48 +67,11 @@ for valores in valores_politica:
         resultado[str(valores)]["repeticion"+str(i)] = s.guardar_kpi()
 
 # Se guardan kpi por repetición en excel
-data_excel = {}
-for valores in valores_politica:
-    #print(f"\nPOLÍTICA {valores}")
-    resultado_kpi = resultado[str(valores)]
-    rows = []
-    for j in range(0, replicas):
-        kpi = resultado_kpi["repeticion"+str(j)]
-        rows.append(list(kpi.values()))
-    
-    df = pd.DataFrame(rows, columns = list(kpi.keys()))
-    #df.to_excel(excel, sheet_name='kpi'+str(valores), index=True)
-
-    nombre_columna = list(df.columns.values)
-    columna = []
-    for i in range(0, len(nombre_columna)):
-        valores_kpi = df[nombre_columna[i]].describe().loc[['mean']].tolist()
-        valores_kpi = map(str,valores_kpi)
-        columna.append(str(''.join(valores_kpi)))
-        data_excel[str(valores)] = columna
-
-    #df.describe().loc[['mean','min','max']].to_excel(excel, sheet_name='kpi'+str(valores), index=True)
+nombre_columna, data_excel = guardar_pares_kpi(valores_politica, resultado, replicas, excel)
 
 
-df2 = pd.DataFrame(data_excel).transpose()
-df2.columns = nombre_columna
-df2.to_excel(excel, sheet_name='Mean', index=True)
-
-for kpi in range(0, len(nombre_columna)):
-    matriz = []
-    for i in range(51):
-        fila = []
-        for j in range(51):
-            if i <= j:
-                tupla = "("+str(i)+", "+str(j)+")"
-                dato = data_excel[tupla][kpi]
-                fila.append(dato)
-            else:
-                fila.append(0)
-        matriz.append(fila)
-
-    df3 = pd.DataFrame(matriz)
-    df3.to_excel(excel, sheet_name="Mean-kpi"+str(kpi), index=True)
+# Se genera matriz por cada kpi en nueva hoja
+guardar_matriz_heatmap_kpi(nombre_columna, rango_s_S, data_excel, excel)
     
 excel.close()
 
