7e7218015b83d5867ca69334e930ceb834313d9c
JosefinaBarra <josefina.barra@uc.cl>
2023-06-07 13:13:46 -0400

    valores producto de csv

--- abrir_archivo.py
+++ abrir_archivo.py
@@ -1,12 +1,29 @@
-import os
+import pandas as pd
 
-demanda_real = {}
-dia = 0
 
-with open("dda_885.txt") as f:
-    contenido = f.read().split('\n')
-    for linea in contenido:
-        separado = linea.split()
-        for i in range(0, len(separado)):
-            demanda_real[dia] = int(separado[i])
-            dia += 1
+def demanda_historica():
+    demanda_real = {}
+    dia = 0
+
+    with open("dda_885.txt") as f:
+        contenido = f.read().split('\n')
+        for linea in contenido:
+            separado = linea.split()
+            for i in range(0, len(separado)):
+                demanda_real[dia] = int(separado[i])
+                dia += 1
+    return demanda_real
+
+
+def data_productos(productos):
+    data = {}
+    archivo = pd.read_csv('items.csv')
+    for producto in productos:
+        data[producto] = {}
+        info = archivo.loc[archivo['item_id'] == producto]
+        data[producto]['nombre'] = info['prod_descr'].tolist()[0]
+        data[producto]['sale_price'] = info['sale_price'].tolist()[0]
+        data[producto]['final_price'] = info['final_price'].tolist()[0]
+        data[producto]['cost'] = info['cost'].tolist()[0]
+        data[producto]['storage_cost'] = info['storage_cost'].tolist()[0]
+    return data
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 from simulacion_diario import Bodega
-from abrir_archivo import demanda_real
+from abrir_archivo import demanda_historica, data_productos
 from guardar_data import guardar_pares_kpi, guardar_matriz_heatmap_kpi, guardar_3d
 
 
@@ -16,8 +16,8 @@ excel = pd.ExcelWriter(
     engine_kwargs={"options": {"strings_to_numbers": True}}
 )
 
-replicas = 1000
-periodos = 30
+replicas = 10
+periodos = 80
 politica = "(s,S)"
 #politica = "EOQ"   
 
@@ -27,13 +27,18 @@ resultado_base = {}
 caso_base = True
 no_mostro_grafico_base = True
 
-rango_s_S = 51
+rango_s_S = 5
+demanda_real = demanda_historica()
+
+productos = [885]
+precios_productos = data_productos(productos)
 
 # SE OBTIENEN PARÁMETROS DEL CASO BASE USANDO DEMANDA HISTÓRICA
 # Demanda real
+'''
 s = Bodega(
         s=np.ceil(np.mean(list(demanda_real.values()))/2),
-        S=np.ceil(np.mean(list(demanda_real.values()))*2),  
+        S=np.ceil(np.mean(list(demanda_real.values()))*2),
         politica=politica,
         periodos=periodos,
         demanda=demanda_real,
@@ -48,47 +53,53 @@ s = Bodega(
 s.run()
 s.grafico()
 print(s.guardar_kpi())
+'''
 
 # BUSCAMOS ÓPTIMO PARA PARÁMETROS
 valores_politica = [(s, S) for s in range(rango_s_S) for S in range(rango_s_S) if s<=S]
 
-print(f"\nESTADÍSTICAS SIMULACIÓN")
-for valores in valores_politica:
-    muestra_grafico = True
-    resultado[str(valores)] = {}
-    #print(f"\nPOLÍTICA {valores}")
-    for i in range(0, replicas):
-        s = Bodega(
-            s=valores[0],
-            S=valores[1],
-            politica=politica,
-            periodos=periodos,
-            demanda=None,
-            precio_venta=6260,
-            costo_pedido=4201,
-            costo_almacenamiento=12,
-            costo_demanda_perdida=1200,
-            tiempo_revision=1, # Tieme que ser >= 1
-            lead_time=7,
-            caso_base=False
-        )
-        s.run()
-        
-        if muestra_grafico:
-            #s.grafico()
-            muestra_grafico = False
+for producto in productos:
+    nombre = precios_productos[producto]['nombre']
+    sale_price = precios_productos[producto]['sale_price']
+    final_price = precios_productos[producto]['final_price']
+    cost = precios_productos[producto]['cost']
+    storage_cost = precios_productos[producto]['storage_cost']
+    for valores in valores_politica:
+        muestra_grafico = True
+        resultado[str(valores)] = {}
+        # print(f"\nPOLÍTICA {valores}")
+        for i in range(0, replicas):
+            s = Bodega(
+                s=valores[0],
+                S=valores[1],
+                politica=politica,
+                periodos=periodos,
+                demanda=None,
+                precio_venta=final_price,
+                costo_pedido=cost,
+                costo_almacenamiento=storage_cost,
+                costo_demanda_perdida=1200,
+                tiempo_revision=1,  # Tieme que ser >= 1
+                lead_time=7,
+                caso_base=False
+            )
+            s.run()
+ 
+            if muestra_grafico:
+                #s.grafico()
+                muestra_grafico = False
 
-        resultado[str(valores)]["repeticion"+str(i)] = s.guardar_kpi()
+            resultado[str(valores)]["repeticion"+str(i)] = s.guardar_kpi()
 
-# Se guardan kpi por repetición en excel
-nombre_columna, data_excel = guardar_pares_kpi(valores_politica, resultado, replicas, excel)
+    # Se guardan kpi por repetición en excel
+    nombre_columna, data_excel = guardar_pares_kpi(valores_politica, resultado, replicas, excel)
 
 
-# Se genera matriz por cada kpi en nueva hoja
-valores_matriz = guardar_matriz_heatmap_kpi(nombre_columna, rango_s_S, data_excel, excel)
-guardar_3d(valores_matriz, nombre_columna)
-    
-excel.close()
+    # Se genera matriz por cada kpi en nueva hoja
+    valores_matriz = guardar_matriz_heatmap_kpi(nombre_columna, rango_s_S, data_excel, excel)
+    guardar_3d(valores_matriz, nombre_columna)
+
+    excel.close()
 
 # Guardo histogramas de kpi en carpetas
 #histogramas_png(valores_politica, replicas)
